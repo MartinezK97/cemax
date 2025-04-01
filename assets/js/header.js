d7e3082d53b9e885de167d3c4d00b8d635f0b414
Kevin Martinez <kevinmartinez08.97@hotmail.com>
2025-04-01 12:20:11 -0300

    Proximo a finalizar

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,7 @@
 document.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     const servicios = document.querySelector("#servicios");
+    const contact = document.querySelector("#contact_header_btn")
     const headerHeight = 8 * 16; // 4rem en píxeles
     const serviciosTop = servicios.getBoundingClientRect().top;
     const triggerPoint = headerHeight; // Cuando el borde superior de #servicios toca el borde inferior de header
@@ -9,5 +10,12 @@ document.addEventListener("scroll", () => {
 
     header.style.transition = "background-color 0.3s ease, backdrop-filter 0.3s ease";
     header.style.backgroundColor = `rgba(251, 194, 107, ${progress * 0.99})`;
+    if (progress >= 1) {
+        contact.style.background = "#fffce5"
+    } else {
+        contact.style.background = "#fbc26b"
+
+    }
+    console.log(progress)
     // header.style.backdropFilter = `blur(${progress * 10}px)`;
 });
